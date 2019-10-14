b6892b76aaa6a6121fcb91055f95fe79457354e1
nhatndm <nhatndm1193@gmail.com>
2019-10-15 00:33:02 +0700

    fixed bug when decode html

--- src/constant/api.js
+++ src/constant/api.js
@@ -1,2 +1,2 @@
 export const fetchPostUrl = (prefix, after) =>
-  `https://www.reddit.com/r/DotA2/${prefix}.json?limit=50&&after=${after}`;
+  `https://www.reddit.com/r/DotA2/${prefix}.json?limit=20&&after=${after}`;
